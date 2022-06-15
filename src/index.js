929cf64c0400788acff7bd89cfdb73f990dd05d1
AWITI0301 <johnoketchawiti@gmail.com>
2022-06-15 11:30:04 +0300

    commit tree d13ff270ec9cabc21f218892f055026773e014c0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
+    const input = document.querySelector("form");
+    input.addEventListener("submit", event => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
   
-}
-
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then( data => {
+          const title = document.querySelector('section#movieDetails h4');
+          const sum = document.querySelector('section#movieDetails p');
+          title.innerText = data.title;
+          sum.innerText = data.summary;
+        })
+        .catch( err => console.log(err))
+  
+      
+     
+    })
+  }
+init()
 document.addEventListener('DOMContentLoaded', init);
